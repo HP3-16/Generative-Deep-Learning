21f582039cc5d3f853a7f9cc4b8d4e452301001c
HP3-16 <hp3.16.bsr@gmail.com>
2023-06-09 15:25:25 +0530

    Decoder added

--- models/AE.py
+++ models/AE.py
@@ -1,5 +1,5 @@
 import numpy as np
-from keras.layers import Input, Conv2D, Flatten, Dense,LeakyReLU, BatchNormalization, Dropout, Conv2DTranspose
+from keras.layers import Input, Conv2D, Conv2DTranspose, Flatten, Dense, Activation, LeakyReLU, BatchNormalization, Dropout, Reshape
 from keras.models import Model
 from keras.backend import int_shape
 from keras.utils import plot_model
@@ -29,6 +29,10 @@ class AutoEncoder():
                     self.build()
     
     def build(self):
+            '''
+            Builds the encoder and decoder of the AE model
+            '''
+    # Encoder        
             encoder_input_layer = Input(shape=self.input_dim,name='Encoder_Input')
             x = encoder_input_layer
             for i in range(self.num_encoder_layers):
@@ -52,5 +56,49 @@ class AutoEncoder():
             encoder_output_layer = Dense(self.z_dim,name='encoder_output')(x)
 
             self.encoder = Model(encoder_input_layer,encoder_output_layer)
+
+    # Decoder
+            decoder_input_layer = Input(shape=self.z_dim,name='decoder_input')
+
+            x = Dense(np.prod(curr_shape))(decoder_input_layer)
+            x = Reshape(curr_shape)(x)
+
+            for i in range(self.num_decoder_layers):
+                    conv_t_layer = Conv2DTranspose(filters=self.d_t_filters[i],
+                                                   kernel_size=self.d_t_kernel[i],
+                                                   strides=self.d_t_strides[i],
+                                                   padding='same',
+                                                   name = 'conv_layer_t_'+str(i)
+                                                   )
+                    x = conv_t_layer(x)
+
+                    if i < self.num_decoder_layers - 1:
+                            x = LeakyReLU()(x)
+
+                            if self.use_batch_norm:
+                                    x = BatchNormalization()(x)
+                            
+                            if self.use_dropout:
+                                    x = Dropout()(x)
+                    else:
+                            x = Activation('sigmoid')
+                    
+            decoder_output_layer = x
+            self.decoder = Model(decoder_input_layer,decoder_output_layer)
+
+            model_input_layer = encoder_input_layer
+            model_output_layer = self.decoder(encoder_output_layer)
+
+            self.model = Model(model_input_layer,model_output_layer)
+    
+
+
+
+
+
+
+            
+
+
 
 
