ebb7494cc17143d4c4dd8016f6bc85a5790697aa
HP3-16 <hp3.16.bsr@gmail.com>
2023-06-28 14:33:06 +0530

    Generator built, yet to be tested

--- models/GAN.py
+++ models/GAN.py
@@ -1,5 +1,5 @@
-import numpy
-from keras.layers import Input,Conv2D,Conv2DTranspose,Flatten,Dense,BatchNormalization,Dropout,Lambda
+import numpy as np
+from keras.layers import Input,Conv2D,Conv2DTranspose,Flatten,Dense,BatchNormalization,Dropout
 from keras.layers import ReLU,LeakyReLU,Activation,UpSampling2D,ZeroPadding2D,Reshape
 from keras.models import Model
 from keras.optimizers import Adam
@@ -8,6 +8,7 @@ from keras.initializers import RandomNormal
 
 class GAN:
     def __init__(self, input_size, discr_conv_filters, discr_activation, discr_dropout_rate, discr_lr,
+                 hidden_layer_dim,
                  gen_conv_filters, gen_activation, gen_dropout_rate, gen_lr, latent_dim):
         self.name = 'GAN'
         self.input_size = input_size
@@ -16,6 +17,7 @@ class GAN:
         self.discr_activation = discr_activation
         self.discr_dropout_rate = discr_dropout_rate
         self.discr_lr = discr_lr
+        self.hidden_layer_dim = hidden_layer_dim
         self.gen_conv_filters = gen_conv_filters
         self.gen_layers = len(gen_conv_filters)
         self.gen_activation = gen_activation
@@ -51,4 +53,27 @@ class GAN:
 
         self.discriminator = Model(discr_input,discr_output,name='Discriminator Model')
 
+    
+    def gen_(self):
+        gen_input = Input(shape=(self.latent_dim,),name='Generator_Input_Layer')
+        x = gen_input
+        x = Dense(units=np.prod(self.hidden_layer_dim))(x)
+        x = BatchNormalization()(x)
+        x = ReLU()(x)
+        x = Reshape(self.hidden_layer_dim)(x)
+        x = Dropout(rate=self.gen_dropout_rate)(x)
+
+        for i in range(self.gen_layers):
+            x = UpSampling2D()(x)
+            x = Conv2D(filters=self.gen_conv_filters,kernel_size=3,strides=2,
+                       padding='same')(x)
+            if i<self.gen_layers-1:
+                x = BatchNormalization()(x)
+                x = LeakyReLU()(x)
+            else:
+                x = Activation('tanh')(x)
+
+        gen_output = x
+        self.generator = Model(gen_input,gen_output)
+                        
 
